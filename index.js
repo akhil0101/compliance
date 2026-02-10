da5eb7bb1b6cec5cba25f216d0755af549de516a
Akhil Kandakatla <akhilkandakatla1997@gmail.com>
2026-02-10 16:18:11 +0530

    Add files via upload
    initial uploads

---
+++ index.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 5; i++) document.cookie = "essential" + i;
+
+for (let i = 0; i < 5; i++) document.cookie = "advertisment" + i;
+
+for (let i = 0; i < 5; i++) document.cookie = "social networking" + i;
+
+for (let i = 0; i < 5; i++) document.cookie = "biometrics" + i;
